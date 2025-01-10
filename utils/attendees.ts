787640d15b34e1e6e1dd0ccf7255b5fce75f414f
Danilo Canguçu <daniloccangucu@gmail.com>
2025-01-10 14:15:09 +0100

    routing & data fetching completed

--- utils/attendees.ts
+++ utils/attendees.ts
@@ -3,25 +3,36 @@ import { db } from "@/db/db";
 import { attendees, events, rsvps } from "@/db/schema";
 import { eq, sql } from "drizzle-orm";
 import { delay } from "./delay";
+import { memoize } from "nextjs-better-unstable-cache";
 
-export const getAttendeesCountForDashboard = async (userId: string) => {
-  await delay(500);
+export const getAttendeesCountForDashboard = memoize(
+  async (userId: string) => {
+    await delay(500);
+    throw new Error("This is an error");
 
-  const counts = await db
-    .select({
-      totalAttendees: sql<number>`count(distinct ${attendees.id})`,
-    })
-    .from(events)
-    .leftJoin(rsvps, eq(rsvps.eventId, events.id))
-    .leftJoin(attendees, eq(attendees.id, rsvps.attendeeId))
-    .where(eq(events.createdById, userId))
-    .groupBy(events.id)
-    .execute();
+    const counts = await db
+      .select({
+        totalAttendees: sql<number>`count(distinct ${attendees.id})`,
+      })
+      .from(events)
+      .leftJoin(rsvps, eq(rsvps.eventId, events.id))
+      .leftJoin(attendees, eq(attendees.id, rsvps.attendeeId))
+      .where(eq(events.createdById, userId))
+      .groupBy(events.id)
+      .execute();
 
-  const total = counts.reduce(
-    (acc, { totalAttendees }) => acc + totalAttendees,
-    0
-  );
+    const total = counts.reduce(
+      (acc, { totalAttendees }) => acc + totalAttendees,
+      0
+    );
 
-  return total;
-};
+    return total;
+  },
+  {
+    persist: true,
+    revalidateTags: () => ["dashboard:attendees"], // put in a CONST
+    suppressWarnings: true,
+    log: ["datacache", "verbose", "dedupe"],
+    logid: "dashboard:attendees",
+  }
+);
